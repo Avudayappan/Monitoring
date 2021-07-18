5320c24907584c522f6013ba63e4b2ab54f912cf
Avudayappan Palanivel <52732216+Avudayappan@users.noreply.github.com>
2021-07-18 14:01:59 +0530

    Update HelloServlet.java

--- src/net/codejava/javaee/HelloServlet.java
+++ src/net/codejava/javaee/HelloServlet.java
@@ -30,7 +30,7 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
-		writer.println("<h1>Hello World from precision - 6</h1>");
+		writer.println("<h1>Hello World from precision - 7</h1>");
 		writer.close();
 	}
 
